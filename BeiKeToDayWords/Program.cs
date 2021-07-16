8e8f78c326a6793b190bcb48c9233cdfad564ad1
WIN-MDI085JTSLE\Administrator <13126506430@163.com>
2021-07-16 20:26:57 +0800

    .

---
+++ BeiKeToDayWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeiKeToDayWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
