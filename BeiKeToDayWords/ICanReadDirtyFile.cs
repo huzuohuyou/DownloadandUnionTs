27c048f3522328909ad4f832e5535878e295c570
WIN-MDI085JTSLE\Administrator <13126506430@163.com>
2021-07-16 20:35:58 +0800

    .

---
+++ BeiKeToDayWords/ICanReadDirtyFile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeiKeToDayWords
+{
+    public interface ICanReadDirtyFile
+    {
+        (List<string>words, List<string> trans ) GetTodayWord();
+    }
+}
